d2792d97dd993c8fe31ccd9e16bda167c602973f
daeyeub <kdy9622@naver.com>
2022-06-04 01:55:27 +0900

    2022-066-04

---
+++ src/fbase.js
@@ -0,0 +1,16 @@
+import  firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlT5SM3GijdUq9UiCSydmAFxEPJF-u0c4",
+  authDomain: "nwitter-v.firebaseapp.com",
+  projectId: "nwitter-v",
+  storageBucket: "nwitter-v.appspot.com",
+  messagingSenderId: "515417718199",
+  appId: "1:515417718199:web:2da021c43f39c47e7e3c32"
+};
+
+const application=firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
+
+export default application
